a744e768556f38cec1c743e2d7fcdc691a86698b
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-28 23:31:08 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -170,7 +170,7 @@ async def amireallyalive(alive):
     user = await alive.client.get_me()
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
-        f"**[Vins](https://github.com/BTRExo/Vinsxid) is Up and Running.**\n\n"
+        f"**[VinsXid](https://github.com/BTRExo/Vinsxid) is Up and Running.**\n\n"
         f"**{alive_text}**\n\n"
         f"{emoji} **Master :** [{user.first_name}](tg://user?id={user.id}) \n"
         f"{emoji} **Modules :** `{len(modules)} Modules` \n"
